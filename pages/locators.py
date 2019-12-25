cb0507661d4716047307ba3e77efee4975b310eb
Your Name <>
2019-12-25 15:22:52 +0200

    Step 4.2.8.

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    
+class LoginPageLocators():
+    L_FORM = (By.CSS_SELECTOR, "#login_form") 
+    R_FORM = (By.CSS_SELECTOR, "#register_form")
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,18 @@
 import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+
+link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
 def test_guest_can_go_to_login_page(browser):
-    link ="http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
     page.should_be_login_link()
     #time.sleep(10)
+    
+def test_check_login_and_register(browser):
+    page =  LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
