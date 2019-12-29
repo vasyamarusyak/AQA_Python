5838e69efdd6a8ce697f3e3cf3b93dcf73854d05
Your Name <>
2019-12-29 17:10:41 +0200

    Step 4.3.11

--- test_main_page.py
+++ test_main_page.py
@@ -2,31 +2,34 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import time
+import pytest
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)   # ініціалізуємо Page Object, передаємо в конструктор экземпляр драйвера и url адрес
+        page.open()                      # відкриваємо сторінку
+        page.go_to_login_page()          # викликаємо метод go_to_login_page. Перейшли на сторінку логіну.
+        time.sleep(5)
+        login_page = LoginPage(browser, browser.current_url)# Ініціалізуємо LoginPage в тілі тесту, для того шоб викликати методи із файлу login_page
+        login_page.should_be_login_page()# із файлу login_page викликаємо метод should_be_login_page
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)   # ініціалізуємо Page Object, передаємо в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # відкриваємо сторінку
-    page.go_to_login_page()          # викликаємо метод go_to_login_page. Перейшли на сторінку логіну.
-    time.sleep(5)
-    login_page = LoginPage(browser, browser.current_url)# Ініціалізуємо LoginPage в тілі тесту, для того шоб викликати методи із файлу login_page 
-    login_page.should_be_login_page()# із файлу login_page викликаємо метод should_be_login_page
-    
-def test_check_login_and_register(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page =  LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+    def test_check_login_and_register(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        page =  LoginPage(browser, link)
+        page.open()
+        page.should_be_login_page()
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = BasketPage(browser, link)
-    page.open()                                           
-    page.test_guest_can_go_to_basket()                     
+    page.open()
+    page.test_guest_can_go_to_basket()
     basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_not_be_any_products_in_busket()     
+    basket_page.should_not_be_any_products_in_busket()
     print("Method 'should_not_be_any_products_in_busket' --- finished")
-    basket_page.should_be_empty_basket_page_with_text()  
-    
+    basket_page.should_be_empty_basket_page_with_text()
 
+#pytest  -v -s --tb=line test_main_page.py -m login_guest - для того щоб
+                                    #запустити тести із міткою login_guest
